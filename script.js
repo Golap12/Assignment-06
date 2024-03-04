c8f39cf2d6166abf4f3f58c8756398bcfcc080a7
Golap12 <golaphasan379@gmail.com>
2024-03-04 11:09:19 +0600

    Mark as read function complate

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 
 const loadPost = async (searchText) => {
-    const res = await fetch (`https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`);
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`);
     const data = await res.json();
     const posts = data.posts;
     displayPosts(posts);
-    handleRead(posts);
 }
 
 
@@ -15,11 +14,11 @@ const displayPosts = posts => {
     postDiv.textContent = '';
 
     posts.forEach(post => {
-        
+
 
         const div = document.createElement("div")
-            div.classList.add('flex', 'gap-5', 'lg:p-10', 'p-2', 'border-blue-500', 'border', 'rounded-3xl', 'bg-[#12132d0c]')
-            div.innerHTML = `
+        div.classList.add('flex', 'gap-5', 'lg:p-10', 'p-2', 'border-blue-500', 'border', 'rounded-3xl', 'bg-[#12132d0c]')
+        div.innerHTML = `
         
             <div class="">
                 <div class="w-[50px] h-[50px] "><img class="rounded-full" src="${post.image}" alt=""></div>
@@ -46,14 +45,14 @@ const displayPosts = posts => {
                         <p class="space-x-2"><i class="fa-regular fa-clock "></i> <span>${post.posted_time} min</span></p>
                     </div>
                     <div class="flex justify-center items-center bg-green-400 rounded-full px-2 py-1">
-                        <button onclick="handleRead(${post.id})"><i class="fa-regular fa-envelope"></i></button>
+                        <button onclick="handleRead('${post.title}', ${post.comment_count})"><i class="fa-regular fa-envelope"></i></button>
                     </div>
                 </div>
             </div>
         `;
 
         postDiv.appendChild(div);
-        
+
         toggleLoadingSpinner(false);
     })
 }
@@ -62,17 +61,28 @@ const displayPosts = posts => {
 
 
 
-const handleRead = (id) => {
 
-    const markRead = document.getElementById('mark-as-read')
-    const div2 = document.createElement("div")
-    div2.classList.add('flex', 'justify-between', 'items-center', 'bg-white', 'p-4', 'rounded-2xl');
-    div2.innerHTML=`
-    <p class="font-medium">Baler hocchena</p>
-    <p class="space-x-1 text-[#868585]"><i class="fa-regular fa-eye "></i><span>jano?</span></p>
+
+const handleRead = (title, commentCount) => {
+
+    const markRead = document.getElementById('mark-as-read');
+
+    const div2 = document.createElement("div");
+    div2.classList.add('flex', 'justify-between', 'items-center', 'bg-white', 'p-3', 'rounded-2xl');
+    div2.innerHTML = `
+    
+    <p class="font-medium">${title}</p>
+    <div class="flex justify-center items-center gap-2">
+        <i class="fa-regular fa-eye "></i>
+        <p class="space-x-1 text-[#868585]">${commentCount}</p>
+    </div>
+
     `
     markRead.appendChild(div2);
 }
+
+
+
 
 
 
@@ -86,7 +96,7 @@ const handleSearch = () => {
     if (searchText) {
         loadPost(searchText);
     }
-    else{
+    else {
         alert("Please enter a valid category")
     }
 
@@ -99,13 +109,13 @@ const handleSearch = () => {
 
 const toggleLoadingSpinner = (isLoading) => {
     const loadingSpinner = document.getElementById('loading')
-    
+
     if (isLoading) {
         loadingSpinner.classList.remove('hidden');
-        
+
     }
-    else{
-        
+    else {
+
         loadingSpinner.classList.add('hidden');
     }
 }
@@ -147,7 +157,7 @@ const latestPost = async () => {
 
         latestPost.appendChild(div);
     })
-    
+
 }
 
 
